9b194932e598af93c67c933e6db8ad419fe432f6
robcgray68 <59897980+robcgray68@users.noreply.github.com>
2020-07-02 20:45:05 -0400

    Add files via upload

--- db/index.js
+++ db/index.js
@@ -1,13 +1,160 @@
-// Connect to DB
 const { Client } = require('pg');
-const DB_NAME = 'change-this-name'
-const DB_URL = process.env.DATABASE_URL || `postgres://${ DB_NAME }`;
-const client = new Client(DB_URL);
 
-// database methods
+const client = new Client('postgres://localhost:5432/gp-dev');
 
-// export
-module.exports = {
+async function createUser({ 
+  username, 
+  password,
+  name,
+  location
+}) {
+  try {
+    const { rows: [ user ] } = await client.query(`
+      INSERT INTO users(username, password, name, location) 
+      VALUES($1, $2, $3, $4) 
+      ON CONFLICT (username) DO NOTHING 
+      RETURNING *;
+    `, [username, password, name, location]);
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateUser(id, fields = {}) {
+  const setString = Object.keys(fields).map(
+    (key, index) => `"${ key }"=$${ index + 1 }`
+  ).join(', ');
+
+  if (setString.length === 0) {
+    return;
+  }
+
+  try {
+    const { rows: [ user ] } = await client.query(`
+      UPDATE users
+      SET ${ setString }
+      WHERE id=${ id }
+      RETURNING *;
+    `, Object.values(fields));
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getAllUsers() {
+  try {
+    const { rows } = await client.query(`
+      SELECT id, username, name, location, active 
+      FROM users;
+    `);
+
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getUserById(userId) {
+  try {
+    const { rows: [ user ] } = await client.query(`
+      SELECT id, username, name, location, active
+      FROM users
+      WHERE id=${ userId }
+    `);
+
+    if (!user) {
+      return null
+    }
+
+    user.posts = await getPostsByUser(userId);
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function createProduct({
+  title,
+  description,
+  photo,
+  price
+}) {
+  try {
+    const { rows: [ product ] } = await client.query(`
+      INSERT INTO posts("authorId", description, photo, price) 
+      VALUES($1, $2, $3, $4)
+      RETURNING *;
+    `, [title, description, photo, price]);
+
+    return product;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateProduct(id, fields = {}) {
+  const setString = Object.keys(fields).map(
+    (key, index) => `"${ key }"=$${ index + 1 }`
+  ).join(', ');
+
+  if (setString.length === 0) {
+    return;
+  }
+
+  try {
+    const { rows: [ product ] } = await client.query(`
+      UPDATE products
+      SET ${ setString }
+      WHERE id=${ id }
+      RETURNING *;
+    `, Object.values(fields));
+
+    return product;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getAllProducts() {
+  try {
+    const { rows } = await client.query(`
+      SELECT *
+      FROM products;
+    `);
+
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// async function getPostsByUser(userId) {
+//   try {
+//     const { rows } = await client.query(`
+//       SELECT * 
+//       FROM posts
+//       WHERE "authorId"=${ userId };
+//     `);
+
+//     return rows;
+//   } catch (error) {
+//     throw error;
+//   }
+// }
+
+module.exports = {  
   client,
-  // db methods
+  createUser,
+  updateUser,
+  getAllUsers,
+  getUserById,
+  createProduct,
+  updateProduct,
+  getAllProducts
+  // getPostsByUser
 }
---
+++ db/seed.js
@@ -0,0 +1,191 @@
+const {  
+  client,
+  createUser,
+  updateUser,
+  getAllUsers,
+  getUserById,
+  createPost,
+  updatePost,
+  getAllPosts,
+  getPostsByUser
+} = require('./index');
+
+async function dropTables() {
+  try {
+    console.log("Starting to drop tables...");
+
+    await client.query(`
+      DROP TABLE IF EXISTS order;
+      DROP TABLE IF EXISTS cart;
+      DROP TABLE IF EXISTS reviews;
+      DROP TABLE IF EXISTS products;
+      DROP TABLE IF EXISTS users;
+    `);
+
+    console.log("Finished dropping tables!");
+  } catch (error) {
+    console.error("Error dropping tables!");
+    throw error;
+  }
+}
+  
+async function createTables() {
+  try {
+    console.log("Starting to build tables...");
+
+    await client.query(`
+      CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username varchar(255) UNIQUE NOT NULL,
+        password varchar(255) NOT NULL,
+        name varchar(255) NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        location varchar(255) NOT NULL,
+        active boolean DEFAULT true,
+        admin boolean DEFAULT false
+      );
+      CREATE TABLE products (
+        id SERIAL PRIMARY KEY,
+        title varchar(255) NOT NULL,
+        description TEXT NOT NULL,
+        photo TEXT,
+        price MONEY NOT NULL
+      );
+      CREATE TABLE reviews (
+        id SERIAL PRIMARY KEY,
+        "productId" INTEGER REFERENCES products(id), 
+        title varchar(255) NOT NULL,
+        stars INTEGER, 
+        review TEXT NOT NULL
+      );
+      CREATE TABLE cart (
+        id SERIAL PRIMARY KEY,
+        "userId" INTEGER REFERENCES users(id),
+        "productId",
+        quantity,
+        status varchar(255)
+      );
+      CREATE TABLE order (
+        id SERIAL PRIMARY KEY,
+        "productId" INTEGER REFERENCES products(id),
+        quantity INTEGER NOT NULL DEFAULT 1,
+        "cartId" INTEGER REFERENCES cart(id)
+      );
+    `);
+
+    console.log("Finished building tables!");
+  } catch (error) {
+    console.error("Error building tables!");
+    throw error;
+  }
+}
+
+
+async function createInitialUsers() {
+  try {
+    console.log("Starting to create users...");
+
+    await createUser({ 
+      username: 'albert', 
+      password: 'bertie99',
+      name: 'Al Bert',
+      location: 'Sidney, Australia' 
+    });
+    await createUser({ 
+      username: 'sandra', 
+      password: '2sandy4me',
+      name: 'Just Sandra',
+      location: 'Ain\'t tellin\''
+    });
+    await createUser({ 
+      username: 'glamgal',
+      password: 'soglam',
+      name: 'Joshua',
+      location: 'Upper East Side'
+    });
+
+    console.log("Finished creating users!");
+  } catch (error) {
+    console.error("Error creating users!");
+    throw error;
+  }
+}
+
+async function createInitialProducts() {
+  try {
+
+    console.log("Starting to create products...");
+    await createProduct({
+      
+    });
+
+    await createProduct({
+      
+    });
+
+    await createProduct({
+      
+    });
+    console.log("Finished creating products!");
+  } catch (error) {
+    console.log("Error creating products!");
+    throw error;
+  }
+}
+
+async function rebuildDB() {
+  try {
+    client.connect();
+
+    await dropTables();
+    await createTables();
+    await createInitialUsers();
+    await createInitialProducts();
+  } catch (error) {
+    console.log("Error during rebuildDB")
+    throw error;
+  }
+}
+
+// async function testDB() {
+//   try {
+//     console.log("Starting to test database...");
+
+//     console.log("Calling getAllUsers");
+//     const users = await getAllUsers();
+//     console.log("Result:", users);
+
+//     console.log("Calling updateUser on users[0]");
+//     const updateUserResult = await updateUser(users[0].id, {
+//       name: "Newname Sogood",
+//       location: "Lesterville, KY"
+//     });
+//     console.log("Result:", updateUserResult);
+
+//     console.log("Calling getAllPosts");
+//     const posts = await getAllPosts();
+//     console.log("Result:", posts);
+
+//     console.log("Calling updatePost on posts[0]");
+//     const updatePostResult = await updatePost(posts[0].id, {
+//       title: "New Title",
+//       content: "Updated Content"
+//     });
+//     console.log("Result:", updatePostResult);
+
+//     console.log("Calling getUserById with 1");
+//     const albert = await getUserById(1);
+//     console.log("Result:", albert);
+
+//     console.log("Finished database tests!");
+//   } catch (error) {
+//     console.log("Error during testDB");
+//     throw error;
+//   }
+// }
+
+
+rebuildDB()
+  //.then(testDB)
+  .catch(console.error)
+  .finally(() => client.end());
