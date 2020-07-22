0f4c5f9c7f45deea499c64debf8d2157aed642c5
suddenx <mattowen333@gmail.com>
2020-07-21 21:48:26 -0400

    for rob

--- src/index.js
+++ src/index.js
@@ -6,11 +6,9 @@ import axios from "axios";
 
 import Modal from "react-modal";
 
+import { fetchProducts } from "./api";
 
-import {
-    fetchProducts,
-} from './api';
-
+import { CartList, SearchBar, SearchResults } from "./components";
 
 import { fetchCards } from "./api";
 
@@ -46,57 +44,58 @@ const App = () => {
       return;
     }
 
-
     const addProductToCart = ({ id, name, price }) => {
-        const nextCart = [...cart];
-        const index = nextCart.findIndex(product => product.id === id);
-      
-        if (index > -1) {
-          nextCart[index].count += 1;
-        } else {
-          nextCart.push({
-            id,
-            name,
-            price,
-            count: 1
-          });
-        }
-      
-        setCart(nextCart);
+      const nextCart = [...cart];
+      const index = nextCart.findIndex((product) => product.id === id);
+
+      if (index > -1) {
+        nextCart[index].count += 1;
+      } else {
+        nextCart.push({
+          id,
+          name,
+          price,
+          count: 1,
+        });
       }
+
+      setCart(nextCart);
+    };
 
     const removeProductFromCart = ({ id }) => {
-        const nextCart = [...cart];
-        const index = nextCart.findIndex(product => product.id === id);
-      
-        if (index === -1) {
-          return;
-        }
-      
-        if (nextCart[index].count === 1) {
-          nextCart.splice(index, 1);
-        } else {
-          nextCart[index].count -= 1;
-        }
-        setCart(nextCart);
+      const nextCart = [...cart];
+      const index = nextCart.findIndex((product) => product.id === id);
+
+      if (index === -1) {
+        return;
       }
- console.log(cart)
+
+      if (nextCart[index].count === 1) {
+        nextCart.splice(index, 1);
+      } else {
+        nextCart[index].count -= 1;
+      }
+      setCart(nextCart);
+    };
+    console.log("hello");
     return (
-        <div id="app">
-            <SearchBar setResults={ setResults } />
-            <SearchResults results={ results }
-                           addProductToCart={ addProductToCart }
-                           removeProductFromCart={ removeProductFromCart } />
-            { cart.length > 0 && <CartList cart={ cart }
-                            addProductToCart={ addProductToCart }
-                            removeProductFromCart={ removeProductFromCart } />}
-        </div>
+      <div id="app">
+        <SearchBar setResults={setResults} />
+        <SearchResults
+          results={results}
+          addProductToCart={addProductToCart}
+          removeProductFromCart={removeProductFromCart}
+        />
+        {cart.length > 0 && (
+          <CartList
+            cart={cart}
+            addProductToCart={addProductToCart}
+            removeProductFromCart={removeProductFromCart}
+          />
+        )}
+      </div>
     );
-}
+  };
 
-
-
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
+  ReactDOM.render(<App />, document.getElementById("root"));
+};
